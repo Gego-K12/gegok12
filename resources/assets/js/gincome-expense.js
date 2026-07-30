52b6c4974a08513e52010b2c3021522a3731b0ce
gurunathan <guru@gegosoft.com>
2026-07-30 14:52:01 +0530

    Changes css

---
+++ resources/assets/js/gincome-expense.js
@@ -0,0 +1,4 @@
+export function registerIncomeExpense(app) {
+    // Register this plugin's Vue components here, e.g.:
+    // app.component('income-expense-example', () => import('./components/Example.vue').then(m => m.default));
+}
